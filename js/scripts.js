226b3cad8631c8cf7faf1e07293c9092a789685e
Nicolas Giovanella <nicolasfgiovanella539@gmail.com>
2024-01-31 07:49:46 -0300

    add prevent default no searchBtn

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,15 @@
 const apiCountry = 'https://flagsapi.com/BE/shiny/64.png';
 const apiKey = '275979b3686fd562e50d17d86312639a';
 
-
 const cityInput = document.querySelector('#city-input')
-const searchBtn = document.querySelector('#csearch')
+const searchBtn = document.querySelector('#search')
+const weatherData = document.querySelector('#weather-data')
 
-cityInput.addEventListener('click', e => {
+//Funções
+
+//Eventos
+searchBtn.addEventListener('click', (e) => {
+  e.preventDefault(); //não enviar formulario
   console.log("clicado")
-  console.log(cityInput);
+  
 })
