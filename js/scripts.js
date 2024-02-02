a2e394cb56b7f134fe0a9db6067c26e32caec840
Nicolas Giovanella <Nicolas@sopal.local>
2024-02-02 11:34:09 -0300

    criando função para pesquisar ao clicar no enter

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,52 @@
 //Variaveise seleção de eventos
-const apiCountry = 'https://flagsapi.com/BE/shiny/64.png';//API fotos de bandeira
+const apiCountry = 'https://flagsapi.com/';//API fotos de bandeira
 const apiKey = '275979b3686fd562e50d17d86312639a';
 
-const cityInput = document.querySelector('#city-input')
-const searchBtn = document.querySelector('#search')
-const weatherData = document.querySelector('#weather-data')
+const cityInput = document.querySelector('#city-input');
+const searchBtn = document.querySelector('#search');
+const weatherData = document.querySelector('#weather-data');
+const iconWeather = document.querySelector('#weather-icon');
 
-const cityElement = document.querySelector('#city')
-const tempElement = document.querySelector('#temperature span')
-const descElement = document.querySelector('#descripition')
+const cityElement = document.querySelector('#city');
+const iconcountry = document.querySelector('#country');
+const tempElement = document.querySelector('#temperature span');
+const descElement = document.querySelector('#description');
 //const weatherData = document.querySelector('#weather-data')
 
 //Funções
 const getWeatherData = async(city) => {
   const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
-
   const res = await fetch(apiWeatherURL);
   const data = await res.json();
-  console.log(data);
+  console.log(res);
   console.log(data.id);
 
   return data; //retornando os dados da api
 }
 
-const showWeatherData = async (city) => { //função que exibe os dados da API
-  const data = await getWeatherData(city);
+const getIconWheater = async (iconCode) => {
+  const iconURL = `https://openweathermap.org/img/wn/${iconCode}.png`;
+  const response = await fetch(iconURL);
+  const icon = await response.blob();
 
-  cityElement.innerText = data.name;
-  tempElement.innerText = parseInt(data.main.temp);
-  //descElement.innerText = data.weather[0].description;
-  weatherData.classList.remove('hide'); //apos buscar os dados, removoa classe com a respota da API
-}
+  return URL.createObjectURL(icon);
+};
+
+const showWeatherData = async (city) => {
+  try {
+    const data = await getWeatherData(city);
+
+    const iconURL = await getIconWheater(data.weather[0].icon);
+    iconWeather.setAttribute("src", iconURL);
+    cityElement.innerText = data.name;
+    tempElement.innerText = parseInt(data.main.temp);
+    descElement.innerText = data.weather[0].description;
+    iconcountry.setAttribute("src", `https://flagsapi.com/${data.sys.country}/shiny/64.png`);
+    weatherData.classList.remove("hide");
+  } catch (error) {
+    console.error("Erro ao buscar os dados da API:", error);
+  }
+};
 
 
 //Eventos
@@ -39,4 +55,13 @@ searchBtn.addEventListener('click', (e) => {
   const city = cityInput.value;
   
   showWeatherData(city);
+})
+
+addEventListener('keypress', (e) => {
+  if(e.key === "Enter"){
+    e.preventDefault(); //não enviar formulario
+    const city = cityInput.value;
+  
+    showWeatherData(city);
+  }
 })
